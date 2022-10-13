1cb34aa54ac16ef258e19d430d422e7f181a7c06
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-13 13:26:26 -0300

    refactor: refactor estructure file app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from 'assets/styles/theme';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-
-    <></>
-
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+          
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
