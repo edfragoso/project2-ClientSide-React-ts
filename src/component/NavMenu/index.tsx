dfcb728e084ea7c0c998ccc4938c25d40e89fa74
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-13 15:30:28 -0300

    feat: add Search

--- src/component/NavMenu/index.tsx
+++ src/component/NavMenu/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as S from "./style";
+import { ReactComponent as Search } from "assets/icons/search.svg";
 
 const NavBar = () => {
   return (
@@ -9,9 +10,11 @@ const NavBar = () => {
         <S.NavMenu>
           <S.NavLink to="/">Inicio</S.NavLink>
           <S.NavLink to="/">Cadastre-se</S.NavLink>
-        <S.NavBtn>
-          <S.NavBtnLink to="/signin">Entrar</S.NavBtnLink>
-        </S.NavBtn>
+          <S.NavLink to="/signin">Entrar</S.NavLink>
+          <S.NavDetails>
+            <Search />
+            <input type="text" placeholder="Buscar" />
+          </S.NavDetails>
         </S.NavMenu>
       </S.Nav>
     </>
