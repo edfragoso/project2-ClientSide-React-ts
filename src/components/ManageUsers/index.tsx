5c1e839ce0253d9901a7e1d0913a07f2be4d75f3
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-14 00:01:45 -0300

    feat: add ManageUser

---
+++ src/components/ManageUsers/index.tsx
@@ -0,0 +1,36 @@
+import { ReactComponent as Add } from "assets/icons/add.svg";
+import { HTMLAttributes } from "react";
+import * as S from "./style";
+import NavBar from "components/NavMenu";
+
+type ManageUsersType = HTMLAttributes<HTMLDivElement>;
+
+type ManageUsersProps = {} & ManageUsersType;
+
+const ManageUsers = ({ ...props }: ManageUsersProps) => {
+  return (
+    <>
+      <NavBar />
+      <S.ManageUsers {...props}>
+        <S.ManageUsersContent>
+          <S.ManageUsersContentAdd>
+            <S.ManageUsersTitle>Cadastro de Usuários</S.ManageUsersTitle>
+            <S.EditForm type="text" placeholder="Nickname" />
+            <S.EditForm type="text" placeholder="Nome" />
+            <S.EditForm type="text" placeholder="Sobrenome" />
+            <S.EditForm type="password" placeholder="Senha" />
+            <S.EditForm type="password" placeholder="Confirmar Senha" />
+            <S.EditForm type="url" placeholder="Imagem" />
+          </S.ManageUsersContentAdd>
+          {/*  {"Componente EditUser"} */}
+        </S.ManageUsersContent>
+        <S.ManageUsersActions>
+          <S.ManageUsersActionsCancel>Cancelar</S.ManageUsersActionsCancel>
+          <S.ManageUsersActionsSave>Salvar Mudanças</S.ManageUsersActionsSave>
+        </S.ManageUsersActions>
+      </S.ManageUsers>
+    </>
+  );
+};
+
+export default ManageUsers;
