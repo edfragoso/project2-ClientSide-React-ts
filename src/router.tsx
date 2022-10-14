8949977641edd6a60aaaebe5354f3f0df449ccb5
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-14 00:00:00 -0300

    feat: add route cadUser

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
+import ManageUsers from "components/ManageUsers";
 
 const Router = () => {
 	return (
 		<Routes>
-			<Route path={RoutePath.LOGIN} element={<Login />} />
 			<Route path={RoutePath.HOME} element={<Home />} />
+			<Route path={RoutePath.LOGIN} element={<Login />} />
+			<Route path={RoutePath.USERS} element={<ManageUsers />} />
 		</Routes>
 	);
 };
