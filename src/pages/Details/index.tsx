94d47c167ecb5e08e7321da939c1fa8ca31f8b45
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-10-25 20:26:27 -0300

    push

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,9 +1,9 @@
 import ProductDetails from "components/ProductDetails";
-// import * as S from "./style";
+
 import { products } from "mocks/products";
 import { useParams } from "react-router-dom";
 import NavBarr from "components/NavBar";
-import './Details.scss';
+import "./Details.scss";
 
 function Details() {
   const { id } = useParams();
@@ -17,19 +17,6 @@ function Details() {
       <main className="boxDetails">
         {product ? <ProductDetails product={product} /> : null}
       </main>
-
-
-
-      {/* <NavBarr />
-      <S.Home>
-        <S.HomeContent>
-          <S.HomeContentItems>
-            <div>
-              {product ? <ProductDetails product={product} /> : null}
-            </div>
-          </S.HomeContentItems>
-        </S.HomeContent>
-      </S.Home> */}
     </>
   );
 }
