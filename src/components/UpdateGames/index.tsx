20750d8b75364ca1d124193f89fde9bbc937f779
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-10-31 14:38:20 -0300

    chore: add component navCrudGames

--- src/components/UpdateGames/index.tsx
+++ src/components/UpdateGames/index.tsx
@@ -1,33 +1,74 @@
-import { NavLink } from 'react-router-dom';
-import './UpdateGames.scss';
+import NavCrudGames from "components/NavCrudGames";
+import { NavLink } from "react-router-dom";
+import "./UpdateGames.scss";
 
 const UpdateGames = () => {
-    return (
+  return (
     <>
-    <div className="BoxCreateGame">
-            <h1>Edite Game</h1>
+      <header>
+        <NavCrudGames />
+      </header>
 
-            <form action="">
-                <label htmlFor="title">Nome do Jogo</label>
-                <input type="text" name="title" id="title" placeholder='Digite o nome do jogo' required/>
-                <label htmlFor="descricao">Descrição</label>
-                <input type="text" name="descricao" id="descricao" placeholder='descricao' required />
-                <label htmlFor="year">Ano de Lançamento</label>
-                <input type="text" name="year" id="year" placeholder='Digite o ano de lançamento' required/>
-                <label htmlFor="image">Adicione a imagem</label>
-                <input type="file" name="image" id="image" placeholder='Adicione a imagem do jogo' required/>
-                <label htmlFor="trailer">Trailer </label>
-                <input type="text" name="trailer" id="trailer" placeholder='Adicione o Trailer do jogo' required/>
-                <label htmlFor="gameplay">Gameplay </label>
-                <input type="text" name="gameplay" id="gameplay" placeholder='Adicione o Gameplay do jogo' required/>
+      <div className="BoxCreateGame">
+        <h1>Edite Game</h1>
 
-                <NavLink to={"/"}>
-                    <button type="submit">Editar</button>
-                </NavLink>
-            </form>
-        </div>
+        <form action="">
+          <label htmlFor="title">Nome do Jogo</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o nome do jogo"
+            required
+          />
+          <label htmlFor="descricao">Descrição</label>
+          <input
+            type="text"
+            name="descricao"
+            id="descricao"
+            placeholder="descricao"
+            required
+          />
+          <label htmlFor="year">Ano de Lançamento</label>
+          <input
+            type="text"
+            name="year"
+            id="year"
+            placeholder="Digite o ano de lançamento"
+            required
+          />
+          <label htmlFor="image">Adicione a imagem</label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            placeholder="Adicione a imagem do jogo"
+            required
+          />
+          <label htmlFor="trailer">Trailer </label>
+          <input
+            type="text"
+            name="trailer"
+            id="trailer"
+            placeholder="Adicione o Trailer do jogo"
+            required
+          />
+          <label htmlFor="gameplay">Gameplay </label>
+          <input
+            type="text"
+            name="gameplay"
+            id="gameplay"
+            placeholder="Adicione o Gameplay do jogo"
+            required
+          />
+
+          <NavLink to={"/"}>
+            <button type="submit">Editar</button>
+          </NavLink>
+        </form>
+      </div>
     </>
-        );
-}
+  );
+};
 
 export default UpdateGames;
