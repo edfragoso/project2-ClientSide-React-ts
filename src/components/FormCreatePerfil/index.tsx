f265f030668d47781e389db7f35ab46f1502427f
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-11-01 16:29:40 -0300

    feat: logic component FormCreatePerfil

--- src/components/FormCreatePerfil/index.tsx
+++ src/components/FormCreatePerfil/index.tsx
@@ -1,25 +1,57 @@
-import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 import "./FormCreatePerfil.scss";
 
 const FormCreatePerfil = () => {
+  const [name, setName] = useState<string>("");
+  const [imagem, setImagem] = useState<string>("");
+
+  const submitForm = (event: any) => {
+    event.preventDefault();
+
+    axios({
+      method: "post",
+      // url: "https://gamingdev.onrender.com/users",
+      data: {
+        name: name,
+        imagem: imagem,
+      },
+    });
+    // .then(() => {
+    //   alert("Perfil criado com sucesso");
+    //   window.location.href = "http://localhost:3000/Perfil";
+    // })
+    // .catch(() => {
+    //   alert("Ops! Algo de errado não esta certo");
+
+    // });
+  };
+
   return (
     <div className="BoxCreatePerfil">
       <h1>Cadastre seu Perfil</h1>
 
-      <form action="">
+      <form onSubmit={submitForm}>
         <label htmlFor="name">Nome</label>
         <input
           type="text"
           name="name"
           id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Digite seu nome."
           required
         />
-        <label htmlFor="image">Adicione sua Foto</label>
-        <input type="file" />
-      <NavLink to={"/perfil"}>
+        <label htmlFor="imagem">Adicione sua Foto</label>
+        <input
+          type="file"
+          name="imagem"
+          id="imagem"
+          value={imagem}
+          onChange={(e) => setImagem(e.target.value)}
+        />
+
         <button type="submit">Enviar</button>
-      </NavLink>
       </form>
     </div>
   );
