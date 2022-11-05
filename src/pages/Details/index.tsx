dd85bd1e392f59273151feb42b6fb1ed64fbb491
BragaJoao <joaogbraga@hotmail.com.br>
2022-11-04 21:05:33 -0300

    Fix checkbox

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -3,11 +3,39 @@ import ProductDetails from "components/ProductDetails";
 import { products } from "mocks/products";
 import { useParams } from "react-router-dom";
 import NavBarr from "components/NavBar";
+/* import { useEffect, useState } from "react"; */
+import axios from "axios";
 import "./Details.scss";
+
+interface Game {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  year: string;
+  score: string;
+  trailer: string;
+  gameplay: string;
+  genreId: string;
+}
 
 function Details() {
   const { id } = useParams();
   const product = products.find((i) => i.id === id);
+/* 
+  useEffect(() => {
+
+    const [game, setGame] = useState<Game>()
+
+    const token = localStorage.getItem('token')
+
+    axios
+      .get<Game[]>("https://gamingdev.onrender.com/games/${id}", {
+        headers:{'Authorization': `Bearer ${token}`}
+      })
+      .then(response => setGame(response.data))
+      .catch((erro) => console.log(erro));
+  }, []); */
 
   return (
     <>
@@ -22,3 +50,7 @@ function Details() {
 }
 
 export default Details;
+function useEffect(arg0: () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
+
