914677ded14c8bd35a03f32adf324acca9e56c92
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-11-06 18:19:16 -0300

    feat: perfil do usuario

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -10,15 +10,19 @@ export interface Profile {
   imageUrl: string;
   title: string;
   userId: string;
+  user: any;
 }
-
 export default function Perfil() {
   const [perfil, setPerfil] = useState<Profile[]>([]);
+
+  const UserId = sessionStorage.getItem("id");
 
   useEffect(() => {
     http
       .get<Profile[]>("profiles")
-      .then((response) => setPerfil(response.data))
+      .then((response) =>
+        setPerfil(response.data.filter((p) => p.user.id == UserId))
+      )
       .catch((error) => console.log(error));
   }, []);
 
@@ -27,7 +31,7 @@ export default function Perfil() {
       <NavBarr />
       <div className="box">
         {perfil.map((item) => (
-          <Item key={item.id} {...item} />
+          <Item key={item.userId} {...item} />
         ))}
       </div>
       <div id="btnPerfil">
